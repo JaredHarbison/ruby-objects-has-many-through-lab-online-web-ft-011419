fb37558392c6bb00a018ad2a57ad7c36113973cb
Jared Harbison <jared.harbison@gmail.com>
2019-01-31 17:45:42 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,14 +1,30 @@
-class Genre 
-  attr_accessor :name
-  @@all = []
+# @@all = []
+# def self.all 
+#     @@all 
+# end 
   
-  def self.all 
-    @@all 
-  end 
-  
-  def initialize(name)
-    @name = name 
-    @@all << self  
-  end 
-  
-end 
+#   def initialize(name)
+#     @name = name 
+#     @@all << self  
+#   end 
+
+class Genre
+  attr_reader :name, :genre
+  attr_accessor :song
+
+   def initialize(name)
+    @name = name
+  end
+
+   def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+   def artists
+    Song.all.map do |song|
+      song.artist
+    end
+  end
+end
