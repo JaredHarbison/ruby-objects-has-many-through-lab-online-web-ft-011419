2f92b06ae3ebc2cb1c742dc7be18399257ed7743
Jared Harbison <jared.harbison@gmail.com>
2019-01-31 17:31:53 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -8,17 +8,12 @@ class Genre
   
   def initialize(name)
     @name = name 
-    @songs = [] 
     @@all << self  
   end 
   
   def songs 
-    @songs 
+    @songs
   end 
-  
-  def add_song(song)
-    @songs << song
-  end
   
   def artists                     
     self.songs.collect do |song| 
