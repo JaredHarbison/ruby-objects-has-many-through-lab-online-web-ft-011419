1793c82933d7d924b671bb1ca6dbf10d7390e359
Jared Harbison <jared.harbison@gmail.com>
2019-01-31 17:29:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,20 +11,5 @@ class Artist
     @songs = []
     @@all << self 
   end
-
-  def new_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-  def songs
-    @songs
-  end
-
-  def genres
-    @songs.collect do |song|
-      song.genre
-    end
-  end
   
 end 
