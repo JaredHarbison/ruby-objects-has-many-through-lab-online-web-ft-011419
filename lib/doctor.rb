0c9c3cdb47d662130e4af8bea222aa6996e595a3
Jared Harbison <jared.harbison@gmail.com>
2019-01-31 17:51:32 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,4 +11,20 @@ class Doctor
     @@all << self 
   end 
   
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+   def patients
+    appointments.map do |appointment|
+      appointment.patients
+    end
+  end
+  
 end 
