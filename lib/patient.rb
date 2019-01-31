768f4103e37ec165de3a8d01cfec802b7d4a3d32
Jared Harbison <jared.harbison@gmail.com>
2019-01-31 17:54:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,10 @@
 class Patient
+  attr_reader :name, :appointment, :doctor
+  @@all = []
 
-   attr_reader :name, :appointment, :doctor
-
-   def initialize(name)
+  def initialize(name)
     @name = name
+    @@all << self 
   end
 
    def new_appointment(doctor, date)
