d22a5c92615685598a478f7d0dd37446a6ed757e
Jared Harbison <jared.harbison@gmail.com>
2019-01-31 17:50:27 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,4 +11,21 @@ class Doctor
     @@all << self 
   end 
   
+  def new_appointment(date, patient)
+    patient = Patient.new(date, self, patient)
+    self.patient.last
+  end
+  
+  #def songs
+  #  Song.all.select do |song|
+  #    song.artist == self
+  #  end
+  #end
+
+  #  def genres
+  #   Song.all.map do |song|
+  #    song.genre
+  #  end
+  #end
+  
 end 
